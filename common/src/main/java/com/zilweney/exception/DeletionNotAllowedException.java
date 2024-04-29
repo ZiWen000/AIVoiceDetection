76ba31bf23284912b6d8d823c605db001bafcd28
zilweney <2672562344@qq.com>
2024-04-29 13:50:57 +0800

    1

---
+++ common/src/main/java/com/zilweney/exception/DeletionNotAllowedException.java
@@ -0,0 +1,9 @@
+package com.zilweney.exception;
+
+public class DeletionNotAllowedException extends BaseException {
+
+    public DeletionNotAllowedException(String msg) {
+        super(msg);
+    }
+
+}
